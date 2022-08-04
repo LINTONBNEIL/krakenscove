227a7345d444af40a05bdea79785c05659560d3a
Neil Linton <lintonbneil@gmail.com>
2022-08-04 13:19:39 -0700

    Add header component and basic structure

---
+++ src/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Header.css';
+
+const Header() => {
+
+
+
+
+  return(
+    <div>
+      <h2>Title+Icon</h2>
+      <nav>
+        <button>Home</button>
+        <button>Search</button>
+        <button>Wishlist</button>
+      </nav>
+    </div>
+  )
+}
+export default Header;
