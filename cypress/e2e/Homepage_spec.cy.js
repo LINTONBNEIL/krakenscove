e894d570c2674ad2ec9ce65f096d0db99670bdb3
Neil Linton <lintonbneil@gmail.com>
2022-08-04 12:32:35 -0700

    Add wishlist, homepage testing files

---
+++ cypress/e2e/Homepage_spec.cy.js
@@ -0,0 +1,38 @@
+describe('HomePage', () => {
+  beforeEach('A user should see the homepage', () => {
+
+  })
+
+  it('Should have a title and an icon', () => {
+    cy.visit('https://example.cypress.io')
+  })
+
+  it('Should have a navbar with three links to different pages of the website', () => {
+  })
+
+  it('Should have a container with a title of features', () => {
+  })
+
+  it('Should show the user a small list of new games', () => {
+  })
+
+  it('Should have error handling for games that do not meet a certain criteria', {
+
+  })
+
+  it('Should have a img, price, and a small button with the ability to like or unlike', () => {
+
+  })
+
+  it('Should update the wishlist/icon when games are liked', () => {
+
+  })
+
+  it('Should update the wishlist/icon when games are unliked', () => {
+
+  })
+
+  it('Should fade out games that are currently liked while using the page', {
+
+  })
+})
---
+++ cypress/e2e/Search_spec.cy.js
@@ -0,0 +1,35 @@
+describe('Search', () => {
+  beforeEach('Should show the user the search/browse page', () => {
+
+  })
+
+  it('Should show the title and icon at the top of the screen', () => {
+    cy.visit('https://example.cypress.io')
+  })
+
+  it('Should show the user the navbar with three button links', () => {
+
+  })
+
+  it('Should show the user a container which holds the form and a list of games', () => {
+
+  })
+
+  it('Should show the user in the games, a games img/title/price/rating/sellers(price-matching)', () => {
+
+  })
+
+  it('Should have a search by title option', () => {
+
+  })
+
+  it('Should have a form that holds several possible options to narrow search', () => {
+
+  })
+
+  it('Should render a new list of games everytime a radio button on the form is clicked', () => {
+
+  })
+
+  
+})
