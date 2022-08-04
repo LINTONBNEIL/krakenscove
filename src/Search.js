bfec8964293fef830a93040255ae544ff5715bbd
Neil Linton <lintonbneil@gmail.com>
2022-08-04 13:15:32 -0700

    Add the skeleton build for the search section

---
+++ src/Search.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './Search.css';
+
+class Search extends Component{
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+
+
+
+  render() {
+    return(
+      <div>
+        <form>
+          <h2>title of form box</h2>
+          <h2>title of form box</h2>
+          <h2>title of form box</h2>
+        </form>
+        <h2>Search Title/button</h2>
+        <p>List of games(another container)</p>
+      </div>
+    )
+  }
+}
+
+export default Search;
